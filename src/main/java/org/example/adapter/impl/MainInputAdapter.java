2c1fa2a1bca66475873d82cf63fbde7c6d866420
n0pr0blem <k.bobkov1408@gmail.com>
2025-02-10 18:14:13 +0300

    new: add commons-lang for isNumeric method and rename FileToListAdapter.java to MainInputAdapter

--- src/main/java/org/example/adapter/impl/MainInputAdapter.java
+++ src/main/java/org/example/adapter/impl/MainInputAdapter.java
@@ -10,13 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public class FileToListAdapter implements MainAdapter {
+public class MainInputAdapter implements MainAdapter {
 
     private List<Employee> employees;
     private List<Manager> managers;
     private List<String> wrongLines;
 
-    public FileToListAdapter() {
+    public MainInputAdapter() {
         employees = new ArrayList<>();
         managers = new ArrayList<>();
         wrongLines = new ArrayList<>();
@@ -24,6 +24,11 @@ public class FileToListAdapter implements MainAdapter {
 
     @Override
     public void adapt(Input input) {
+        input.getData()
+                .forEach(this::redirectPerson);
+    }
+
+    private void redirectPerson(String person) {
 
     }
 
