757d54db1c218eb4deb532e66fcdd52667edb548
n0pr0blem <k.bobkov1408@gmail.com>
2025-02-10 15:58:54 +0300

    new: Create base models and add lombok dependency

--- src/main/java/org/example/model/Employee.java
+++ src/main/java/org/example/model/Employee.java
@@ -1,5 +1,15 @@
 package org.example.model;
 
-public class Employee {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+    private int managerId;
 }
--- src/main/java/org/example/model/Manager.java
+++ src/main/java/org/example/model/Manager.java
@@ -1,5 +1,14 @@
 package org.example.model;
 
-public class Manager {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Manager {
+    private int id;
+    private String name;
+    private String departmentName;
 }
